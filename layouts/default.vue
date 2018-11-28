10e073330cce8399eb6b3bb47d63e2ff1cb13e6b
minssd <minssd@jiran.com>
2018-11-28 14:17:00 +0900

    내용 추가 중

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
-    <nuxt/>
+    <div class="header">
+      Vuejs 소개
+      <br>
+      <span class="header sub-title">{{ title }}</span>
+      <hr>
+    </div>
+    <div class="containor">
+      <div class="left-nav">
+        <ul>
+          <li>vue js 소개</li>
+        </ul>
+      </div>
+      <div class="contents">
+        <nuxt/>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      title: 'vuejs 소개'
+    }
+  }
+}
+</script>
+
 <style>
+.header {
+  font-size: 30px;
+  padding: 20px 10px 10px 20px;
+}
+.header hr {
+  border: solid 2px orange;
+}
+.header.sub-title {
+  font-size: 20px;
+}
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -15,41 +50,5 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
---
+++ pages/content001.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>title : 뷰 인스턴스</p>
+    <p>vue 인스턴스 생성 후 컴포넌트를 사용하여 페이지 표시</p>
+    <p>vue 인스턴스의 data에 object를 등록하면 반응형 데이터가 됨</p>
+    <p>인스터스의 라이프 사이클 create, mounted(onLoad의 역할), updated, distroyed</p>
+    
+  </div>
+</template>
+<style>
+</style>
